b4ccf8175fe58d24be99fc9f2fefed0c4d0e47c1
Kevin <kevin.hall704@gmail.com>
2019-07-25 15:35:22 +0000

    about to deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy
+    ]
+    
+    
  def index
    @articles = Article.all  
  end
@@ -10,7 +14,7 @@ class ArticlesController < ApplicationController
  end
  
  def edit
-    @article = Article.find(params[:id])
+    
  end
      
      
@@ -26,7 +30,7 @@ class ArticlesController < ApplicationController
     
    def update
 
-    @article = Article.find(params[:id])
+   
     
     if @article.update(article_params)
     
@@ -42,19 +46,22 @@ class ArticlesController < ApplicationController
 
    end
     def show
-        @article = Article.find(params[:id])
+       
     end
-    
+     def destroy
+        
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted"
+        redirect_to articles_path
+    end
     private
-
+    
+        def set_article
+          @article = Article.find(params[:id])
+        end
         def article_params
           params.require(:article).permit(:title, :description)
         end
 end
 
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-        flash[:notice] = "Article was successfully deleted"
-        redirect_to articles_path
-    end
+   
